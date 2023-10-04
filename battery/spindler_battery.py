a8f081d1f0b9440caff38137130766398ecd2a89
apha-pesso <mutiuayo99@gmail.com>
2023-10-04 14:58:51 +0100

    Create the battery class

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,18 +1,14 @@
 # Spindler Battery Class
 
-from abc import ABC
-from car import Car
-from datetime import datetime
+from serviceable import Serviceable
 
-
-class SpindlerBattery(Car, ABC):
+class SpindlerBattery(Serviceable):
     '''Spindler Battery Class'''
-    def __init__(self, last_service_date):
-        super().__init__(last_service_date)
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
 
-    def battery_should_be_serviced(self):
+    def needs_service(self):
+        '''Returns True if the battery is more than 2 years old, False otherwise'''
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date():
-            return True
-        else:
-            return False
+        return service_threshold_date < self.current_date
