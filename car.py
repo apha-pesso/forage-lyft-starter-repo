07fa754f1c52003a84e2f581b58cd57a9aea971e
apha-pesso <mutiuayo99@gmail.com>
2023-10-06 22:04:40 +0100

    Integrate tyre to the car class

--- car.py
+++ car.py
@@ -6,9 +6,10 @@ from serviceable import Serviceable
 class Car(Serviceable):
     '''Car class'''
 
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tyres):
         self.engine = engine
         self.battery = battery
+        self.tyres = tyres
 
     def needs_service(self):
         '''Returns True if the engine or battery needs service, False otherwise'''
