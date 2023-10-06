77e81b2c5c3458218d29f2e26205efe195267c02
apha-pesso <mutiuayo99@gmail.com>
2023-10-06 21:36:59 +0100

    Revert "Review spindler battery to 3 year service period"
    This reverts commit 080cbe003e854aa49a34b04311f73ec7e31d9e67.

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -11,7 +11,7 @@ class SpindlerBattery(Serviceable):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        '''Returns True if the battery is more than 3 years old, False otherwise'''
+        '''Returns True if the battery is more than 2 years old, False otherwise'''
         service_threshold_date = self.last_service_date.replace(
-            year=self.last_service_date.year + 3)
+            year=self.last_service_date.year + 2)
         return service_threshold_date < self.current_date
--- test/test_battery.py
+++ test/test_battery.py
@@ -37,13 +37,13 @@ class TestSpinlder(unittest.TestCase):
 
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 4)
+        last_service_date = today.replace(year=today.year - 3)
         battery = SpindlerBattery(today, last_service_date)
         self.assertTrue(battery.needs_service())
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 1)
         battery = SpindlerBattery(today, last_service_date)
         self.assertFalse(battery.needs_service())
 
